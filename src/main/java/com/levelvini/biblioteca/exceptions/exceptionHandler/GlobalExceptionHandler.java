5679b7b520bccdc18699753854249a8764725952
Levelvini <viniciusfc29@gmail.com>
2025-02-18 10:40:44 -0300

    feat: more exceptions suported

--- src/main/java/com/levelvini/biblioteca/exceptions/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/levelvini/biblioteca/exceptions/exceptionHandler/GlobalExceptionHandler.java
@@ -4,18 +4,29 @@ import com.levelvini.biblioteca.exceptions.EmptyDataException;
 import com.levelvini.biblioteca.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErroResponse> Exception(Exception ex){
         ErroResponse erro = new ErroResponse(LocalDateTime.now(), ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.toString());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro); 
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErroResponse> handlerHttpMessageNorReadablrException(HttpMessageNotReadableException ex){
+        ErroResponse erro = new ErroResponse(LocalDateTime.now(),"Erro na leitura do Json",HttpStatus.BAD_REQUEST.toString());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -30,4 +41,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
     //EmptyDataException é desnecessária, podendo ser substituída pelo ResourseNotFoundException! o Uso é para fins de aprendizado!
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
